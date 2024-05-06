066a9fff71ea5924c3a2e1cb345f39b3c0a3e9fd
DavideBrambilla98 <d.brambilla7@studenti.unibg.it>
2024-05-06 20:15:05 +0200

    Modifica alla gestione del DB
    Aggiunte le query per poter filtrare le altre tabelle

--- workspace/gestioneDB.php
+++ workspace/gestioneDB.php
@@ -1,19 +1,56 @@
 <?php      
-	function readPatologieFromDb ($cod, $nome, $criticità, $cronica, $mortale) : string {
-        $sql = "SELECT Codice, Nome, Criticità, Cronica, Mortale FROM Patologie WHERE 1=1";
+	function readPatologieFromDb ($cod, $nome, $criticita, $cronica, $mortale) : string {
+        $sql = "SELECT Codice, Nome, Criticita, Cronica, Mortale FROM Patologie WHERE 1=1";
             
         if ($cod != "")
             $sql .= " AND Codice = :cod";
         if ($nome != "")
             $sql .= " AND Nome LIKE :nome";
-        if ($criticità != "")
-            $sql .= " AND Criticità = :criticità";
+        if ($criticita != "")
+            $sql .= " AND Criticita = :criticita";
         if ($cronica != "")
             $sql .= " AND Cronica = :cronica";
         if ($mortale != "")
             $sql .= " AND Mortale = :mortale";
 
         return $sql;
-    
 	}
+
+
+    function readOspedaliFromDb ($codStruttura, $nomeStruttura, $indirizzoStruttura, $comuneStruttura, $direttoreSanitario) : string {
+        $sql = "SELECT CodiceStruttura, DenominazioneStruttura, Indirizzo, Comune, DirettoreSanitario FROM Ospedali WHERE 1=1";
+            
+        if ($codStruttura != "")
+            $sql .= " AND CodiceStruttura LIKE :codStruttura";
+        if ($nomeStruttura != "")
+            $sql .= " AND DenominazioneStruttura LIKE :nomeStruttura";
+        if ($indirizzoStruttura != "")
+            $sql .= " AND Indirizzo LIKE :indirizzoStruttura";
+        if ($comuneStruttura != "")
+            $sql .= " AND Comune LIKE :comuneStruttura";
+        if ($direttoreSanitario != "")
+            $sql .= " AND DirettoreSanitario LIKE :direttoreSanitario";
+    
+        return $sql;
+    }
+
+    function readPersoneFromDb ($cf, $nome, $cognome, $dataNascita, $luogoNascita ,$indirizzo) : string {
+        $sql = "SELECT  codFiscale,nome, cognome, dataNascita, nasLuogo, indirizzo FROM Persone WHERE 1=1";
+            
+        if ($cf != "")
+            $sql .= " AND codFiscale LIKE :cf";
+        if ($nome != "")
+            $sql .= " AND nome LIKE :nome";
+        if ($cognome != "")
+            $sql .= " AND cognome LIKE :cognome";
+        if ($dataNascita != "")
+            $sql .= " AND dataNascita LIKE :dataNascita";
+        if ($luogoNascita != "")
+            $sql .= " AND nasLuogo LIKE :luogoNascita";
+            if ($indirizzo != "")
+            $sql .= " AND indirizzo LIKE :indirizzo";
+    
+        return $sql;
+    }
+    
 ?>
