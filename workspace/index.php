ad92a6b91da4e53b286827ff337b3164012027c8
DavideBrambilla98 <d.brambilla7@studenti.unibg.it>
2024-05-21 12:12:36 +0200

    Modificato stile tabella index

--- workspace/index.php
+++ workspace/index.php
@@ -150,7 +150,7 @@
 
                 if ($statoPDO->rowCount() > 0) {
 
-                    echo "<table id='tabella'><tr><th>Paziente</th><th>CF paziente</th><th>Nome ospedale</th><th>Motivo</th><th>Data</th><th>Durata</th><th>Costo</th><th></th><th></th></tr>";
+                    echo "<table id='tabella'><tr><th>Paziente</th><th>CF paziente</th><th>Nome ospedale</th><th>Motivo</th><th>Data</th><th>Durata</th><th>Costo</th><th>Azioni</th></tr>";
 
                     // output data of each row
 
@@ -168,16 +168,14 @@
                         <td>".$row["Durata"]."</td>
                         <td>".$row["Costo"]."</td>
                         <td> 
-                        <form class= 'pulsCrud' id='delete-form-{$row["CodiceRicovero"]}' action='delete.php' method='post'>
-                            <input type='hidden' name='CodiceRicovero' value='{$row["CodiceRicovero"]}'>
-                            <input type='hidden' name='delete' value='1'>
-                            <button type='button' onclick='confirmDelete(\"{$row["CodiceRicovero"]}\")'><i class='fa-solid fa-trash'></i></button>
-                        </form>
-                        </td>
-                        <td>
                         <form class= 'pulsCrud' action='update.php' method='get'>
                             <input type='hidden' name='CodiceRicovero' value='".$row["CodiceRicovero"]."'>
                             <button type='submit' name='update'><i class='fa-solid fa-pen'></i></button>
+                        </form>
+                        <form class= 'pulsCrud' id='delete-form-{$row["CodiceRicovero"]}' action='delete.php' method='post'>
+                        <input type='hidden' name='CodiceRicovero' value='{$row["CodiceRicovero"]}'>
+                        <input type='hidden' name='delete' value='1'>
+                        <button type='button' onclick='confirmDelete(\"{$row["CodiceRicovero"]}\")'><i class='fa-solid fa-trash'></i></button>
                         </form>
                         </td>
                         </tr>";
